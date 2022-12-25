00a0116f77eae02e956bd5c10be7326b3842b251
unknown <kamil.jedrzejewski@students.plymouth.ac.uk>
2022-12-25 00:28:48 +0000

    Created the model for POST, added it to admin area, created first blog post, added authored by feature

--- blogProject/blogApp/models.py
+++ blogProject/blogApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+# used to convert an object of a model into a string representation. This method is called when you use the str() function on an object of a model, or when you print an object of a model.
+    def __str__(self):
+        return self.title + '-' + str(self.author)

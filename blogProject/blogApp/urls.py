ad439b0b4149c699970360331eb7e0a4528b3173
unknown <kamil.jedrzejewski@students.plymouth.ac.uk>
2022-12-25 01:04:07 +0000

    created a class based home view to display blog posts, added a url redirecting and a for loop in the home template

--- blogProject/blogApp/urls.py
+++ blogProject/blogApp/urls.py
@@ -1,7 +1,9 @@
 
 from django.urls import path
-from . import views
-
+#from . import views
+from .views import home_View
 urlpatterns = [
-    path('', views.home, name="home"),
+    #path('', views.home, name="home"),
+    path('', home_View.as_view(), name="home"),
+
 ]
--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-def home(request):
-    return render(request, 'home.html', {})
+#def home(request):
+ #   return render(request, 'home.html', {})
 
+class home_View(ListView):
+    model = Post
+    template_name = 'home.html'

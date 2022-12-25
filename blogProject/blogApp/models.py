1ad9e711ff180fded42c548aea991a4465127bfc
unknown <kamil.jedrzejewski@students.plymouth.ac.uk>
2022-12-25 22:57:31 +0000

    appended functionality to add blog posts from a template

--- blogProject/blogApp/models.py
+++ blogProject/blogApp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=255)
@@ -9,3 +9,8 @@ class Post(models.Model):
 # used to convert an object of a model into a string representation. This method is called when you use the str() function on an object of a model, or when you print an object of a model.
     def __str__(self):
         return 'Author:' + self.title + '-' + str(self.author)
+
+#this allows user to post, redirect to created post
+    def get_absolute_url(self):
+       # return reverse('post_Detail', args=(str(self.id)) )
+       return reverse('home')
--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 #def home(request):
@@ -10,5 +10,12 @@ class home_View(ListView):
     template_name = 'home.html'
 
 class detail_View(DetailView):
-    model = Post
+    model = Post 
     template_name = 'post_Detail.html'
+
+class post_Add(CreateView):
+    model = Post
+    template_name ="post_Add.html"
+    fields = ("__all__")
+
+

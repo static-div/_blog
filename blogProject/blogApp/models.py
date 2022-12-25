175ee01a71673f431291e48db72e922fb2b56975
unknown <kamil.jedrzejewski@students.plymouth.ac.uk>
2022-12-25 00:33:14 +0000

    fixed an error caused by not converting the object into a str(object)

--- blogProject/blogApp/models.py
+++ blogProject/blogApp/models.py
@@ -8,4 +8,4 @@ class Post(models.Model):
     body = models.TextField()
 # used to convert an object of a model into a string representation. This method is called when you use the str() function on an object of a model, or when you print an object of a model.
     def __str__(self):
-        return self.title + '-' + str(self.author)
+        return 'Author:' + self.title + '-' + str(self.author)

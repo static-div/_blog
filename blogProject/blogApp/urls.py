ab4a6f1fc6c5468667ffcad4a3c8f27f2e1311d3
unknown <kamil.jedrzejewski@students.plymouth.ac.uk>
2022-12-26 04:06:45 +0000

    implemented delete post feature

--- blogProject/blogApp/urls.py
+++ blogProject/blogApp/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 #from . import views
-from .views import home_View, detail_View, post_Add, update_View
+from .views import home_View, detail_View, post_Add, update_View, delete_View
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', home_View.as_view(), name="home"),
     path('blog/<int:pk>', detail_View.as_view(), name="post_Detail"),
     path('create_post/', post_Add.as_view(), name="post_Create"),
-   path('article/edit/<int:pk>', update_View.as_view(), name="post_Update" )
-    #path('edit_post/')
+   path('article/edit/<int:pk>', update_View.as_view(), name="post_Update" ),
+   path('delete/<int:pk>', delete_View.as_view(), name="post_Delete")
+    
 ]   
--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from .forms import createForm
-
+from django.urls import reverse_lazy
 #def home(request):
  #   return render(request, 'home.html', {})
 
 class home_View(ListView):
     model = Post
     template_name = 'home.html'
+    ordering = ['-id']
 
 class detail_View(DetailView):
     model = Post 
@@ -28,3 +29,8 @@ class update_View(UpdateView):
     model = Post
     template_name = 'post_Update.html'
     fields = ['title', 'body']
+
+class delete_View(DeleteView):
+        model = Post
+        template_name = 'post_Delete.html'
+        success_url = reverse_lazy('home')

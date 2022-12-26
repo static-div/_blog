e3a17466634926280925631c63de6827fe7b1472
unknown <kamil.jedrzejewski@students.plymouth.ac.uk>
2022-12-26 03:36:18 +0000

    implemented update feature for blog posts

--- blogProject/blogApp/urls.py
+++ blogProject/blogApp/urls.py
@@ -1,11 +1,12 @@
 
 from django.urls import path
 #from . import views
-from .views import home_View, detail_View, post_Add
+from .views import home_View, detail_View, post_Add, update_View
 urlpatterns = [
     #path('', views.home, name="home"),
     path('', home_View.as_view(), name="home"),
     path('blog/<int:pk>', detail_View.as_view(), name="post_Detail"),
-    path('create_post/', post_Add.as_view(), name="post_Create")
-
-]
+    path('create_post/', post_Add.as_view(), name="post_Create"),
+   path('article/edit/<int:pk>', update_View.as_view(), name="post_Update" )
+    #path('edit_post/')
+]   
--- blogProject/blogApp/views.py
+++ blogProject/blogApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post
+from .forms import createForm
 
 #def home(request):
  #   return render(request, 'home.html', {})
@@ -13,9 +14,17 @@ class detail_View(DetailView):
     model = Post 
     template_name = 'post_Detail.html'
 
+
+
 class post_Add(CreateView):
     model = Post
+    form_class = createForm
     template_name ="post_Add.html"
-    fields = ("__all__")
+    
+    #dont need to use these anymore because I created a form class in forms.py
+    #fields = ("__all__")
 
-
+class update_View(UpdateView):
+    model = Post
+    template_name = 'post_Update.html'
+    fields = ['title', 'body']
